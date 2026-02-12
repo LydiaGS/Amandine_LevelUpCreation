cf359393feb09579e7a52570ef01a1ffe61a58b3
amandine letellier <letellier.ama@gmail.com>
2026-02-12 23:14:14 +0100

    flecheversle haut

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -242,3 +242,19 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
